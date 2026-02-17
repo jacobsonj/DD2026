78fad4d70cb885f101e23dc40ada5b32f0089822
jacobsonj <justinjacobson0118@gmail.com>
2026-02-17 12:14:59 -0500

    Week6 Adding Data

---
+++ Week6/server.js
@@ -0,0 +1,75 @@
+//1. setup a node app with command: npm init  
+//2. install express with command: npm install express
+//3. create a file named server.js and add the following code
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const hbs = require('express-handlebars');
+
+app.engine('handlebars', hbs.engine());
+app.set('view engine', 'handlebars');
+//app.set('views', './views');
+
+// the path module is used to work with file and directory paths
+const path = require('path');
+
+
+app.use(express.static( path.join(__dirname, 'static'))); //this line tells express to serve static files from the 'static' directory
+
+//data
+const directory = require('./data/directory.json');
+
+console.log(directory);
+
+
+//generate routese
+app.get('/', (req, res) => {
+    //sendFile is used to send a file as a response to the client
+    let filePath = path.join(__dirname, 'static', 'homepage.html');
+    res.sendFile(filePath);
+});
+
+// app.get('/about', (req, res) => {
+//     //sendFile is used to send a file as a response to the client
+//     let filePath = path.join(__dirname, 'static', 'about.html');
+//     res.sendFile(filePath);
+// });
+
+//rendering templates
+app.get('/home', (req, res) => {
+    res.render('home', {title: "My Webstite's homepage"});
+});
+app.get('/about', (req, res) => {
+    res.render('about', {title: "My Webstite's about page"});
+});
+
+
+app.get('/directory/:id', (req, res) => {
+    res.send("this is a get response from api/items")
+});
+
+
+app.get('/api/items', (req, res) => {
+    res.send("this is a get response from api/items")
+});
+app.post('/api/items', (req, res) => {
+    res.send("this is a post response from api/items")
+});
+app.put('/api/items/:id', (req, res) => {
+    res.send("this is a put response from api/items")
+});
+app.delete('/api/items/:id', (req, res) => {
+    res.send("this is a delete response from api/items")
+});
+
+
+//Directory route
+app.get('/directory', (req, res) => {
+    res.render('directory', {directory: directory});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
